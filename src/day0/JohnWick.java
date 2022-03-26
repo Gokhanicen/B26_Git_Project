a584242043e86c830369e816069542966e7ae5e9
Gokhanicen <98862684+Gokhanicen@users.noreply.github.com>
2022-03-26 16:28:59 -0400

    Create JohnWick.java

---
+++ src/day0/JohnWick.java
@@ -0,0 +1,15 @@
+package day0;
+
+public class JohnWick{
+  
+  public static void main(String [] args){
+    
+    System.out.println("John Wick hacked the project"):
+  }
+  
+}
+
+// John Wick has hacked our project
+
+// We have new changes in the remote
+
