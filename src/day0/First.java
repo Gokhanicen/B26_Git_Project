8940440ff9eec7fdf3697a386a4effb76da2c48d
gokhanicen <icengokhan@gmail.com>
2022-03-26 16:03:58 -0400

    Added First file

---
+++ src/day0/First.java
@@ -0,0 +1,16 @@
+package day0;
+
+public class First {
+
+    public static void main(String[] args) {
+
+
+        //TODO: Works on this tomorrow
+        // add some cool code
+
+        System.out.println("I will learn python");
+
+    }
+
+
+}
